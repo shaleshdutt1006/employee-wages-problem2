b5b37ea1f3cc894e4759d289377df15c613d5d9e
shalesh1006 <shaleshdutt1006@gmail.com>
2022-09-01 11:31:10 +0530

    Refactored : Checking Employee is present or absent

--- src/EmployeeWages.java
+++ src/EmployeeWages.java
@@ -3,6 +3,15 @@ public class EmployeeWages {
 
     public static void main(String[] args) {
 
-        System.out.println("Welcome to Employee Wage Computation Program on master branch");
+        {
+
+            double empCheck = Math.floor(Math.random() * 10) % 2;
+
+            if (empCheck == 1) {
+                System.out.println("Employee is present");
+            } else
+                System.out.println("Employee is absent");
+
+        }
     }
 }
