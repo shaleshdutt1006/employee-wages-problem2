a155d5a4bbda3307387493fa81e411b65872e9fb
shalesh1006 <shaleshdutt1006@gmail.com>
2022-09-01 11:28:11 +0530

    Created : Adding the master branch

---
+++ src/EmployeeWages.java
@@ -0,0 +1,8 @@
+public class EmployeeWages {
+
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Employee Wage Computation Program on master branch");
+    }
+}
